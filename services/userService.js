eb75cbec6815d67346d8220c824b3b9e0d42eccc
shallowlong <shallowlong@gmail.com>
2025-08-27 10:41:30 +0800

    bcrypt is replaced by bcryptjs due to the resource issue on server.. add entry point for laoxue host

--- services/userService.js
+++ services/userService.js
@@ -1,5 +1,5 @@
 const debug = require('debug')('fangzhibao-supplier-mgmt:services/UserService')
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcryptjs')
 const { sequelize } = require('../database')
 
 const userModel = sequelize.models.User
